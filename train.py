49d580181fa358feb98483b0259f616cb8e1daaf
readicculus <yuval@yuvalboss.com>
2019-06-19 21:15:14 -0700

    lightning just hit real close so this is one of them just incase moments

--- train.py
+++ train.py
@@ -1,19 +1,32 @@
 import argparse
+import logging
 import os
 import signal
 import subprocess
 import atexit
 import sys
+from io import StringIO
 
 parser = argparse.ArgumentParser(description='Train darknet.')
 parser.add_argument('params', nargs='*')
 args = parser.parse_args()
 params = args.params
 
-cmd = " ".join(['./darknet'] + ["detector", "train"] + params)
-# cmd = "./darknet detector train " + " ".join(params)
-result = subprocess.call(cmd, shell=True, stdout=subprocess.PIPE)
-# proc = subprocess.run(cmd)
+arg_arr = ['./darknet'] + ["detector", "train"] + params + [" -gpus 0"]
+cmd = " ".join(arg_arr)
+# result = subprocess.call(cmd, shell=True, stdout=subprocess.PIPE)
 
 
+from subprocess import Popen, PIPE, STDOUT
+with Popen(arg_arr, stdout=PIPE, stderr=STDOUT, bufsize=1,
+           universal_newlines=True) as p, StringIO() as buf:
+    for line in p.stdout:
+        print(line, end='')
+        buf.write(line)
+    output = buf.getvalue()
+rc = p.returncode
 
+
+# for line in iter(process.stdout.readline, ''):  # replace '' with b'' for Python 3
+#         sys.stdout.write(line)
+# exitcode = process.wait() # 0 means success
